c7a04bec138bab589acdb6c825616de9e3a8ffc8
Wolfey <61946367+Arsdeep@users.noreply.github.com>
2023-08-02 12:18:59 +0530

    second
    Changed the menu

---
+++ script.js
@@ -0,0 +1,9 @@
+const button = document.getElementById("myButton");
+
+
+const messageBox = document.getElementById("messageBox");
+
+
+button.addEventListener("click", function() {
+  alert("Work in Progress :P")
+});

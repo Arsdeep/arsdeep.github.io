f2e9dd54d25d6cc8c4d9991a1fd0c2c0c3b03b7a
Wolfey <61946367+Arsdeep@users.noreply.github.com>
2023-08-04 14:57:41 +0530

    removed everything

--- script.js
+++
@@ -1,9 +0,0 @@
-const button = document.getElementById("myButton");
-
-
-const messageBox = document.getElementById("messageBox");
-
-
-button.addEventListener("click", function() {
-  alert("Work in Progress :P")
-});
